0125d526d36848fd1961046f21b2676dd0e28fd5
francesco olivato <f.olivato.9@gmail.com>
2023-01-30 13:25:04 +0100

    Added a video to frame script converter

---
+++ video_to_frames.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import time
+from tqdm import tqdm
+
+start_time = time.time()
+
+path = "./data/advio-02/iphone/"
+file_extension = ".mov"
+filename = path + "frames"+ file_extension
+
+vid = cv2.VideoCapture(filename)
+fps = vid.get(cv2.CAP_PROP_FPS)
+frame_count = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+count = 0
+
+print(f"\nExtracting {filename} ...\n")
+while vid.isOpened():
+    for i in tqdm(range(frame_count+1)):
+      ret,frame = vid.read()
+
+      if not os.path.exists(path+"frames/"):
+          os.mkdir(path+"frames/")
+      
+      try:
+        cv2.imwrite(path+"frames/"+f"frame_{count:05}.jpg", frame)
+      except Exception as e:
+        pass
+
+      count = count + 1
+      if cv2.waitKey(10) & 0xFF == ord('q'):
+          break
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'\nIt took {elapsed_time}s to extract {frame_count/fps}s of video')
+print(f"Done, check the images at {path+'frames'}")
+vid.release()
