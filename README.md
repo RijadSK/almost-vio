bd37901d420589db8a3d8048cbb5fd0cb1e5d038
francesco olivato <f.olivato.9@gmail.com>
2023-02-08 21:39:52 +0100

    Added resamplig to 50Hz

--- README.md
+++ README.md
@@ -22,12 +22,20 @@ graph LR;
 
 # Dataset
 
-source: https://github.com/AaltoVision/ADVIO (francesco)
+Dataset source: https://github.com/AaltoVision/ADVIO
+
+## Preprocessing
+
+1. Resample the 60Hz video frames to 50Hz in order to have exactly 2 intertial data for each frame.
+
+   Each timestamp in the 50Hz sampling is associated with the frame of the closest timestamp of the 60Hz samplig.
+
+2. Sync the timestamp on the accellerometer, gyroscope, video frames and ground truth files
 
 ## Utils
 
 - **video_to_frame.py**: it extracts the frames from the video in order to prepare the data
-- **sanitize_labels.py**: it checks if the frames extrated by _video_to_frame.py_ are present in the labels (_frames.csv_) in order to have coherent frames and labels
+- **data_setup.py**: resample the video to a lower framerate (50Hz) and couple it with the correct frame
 
 # Reference
 
---
+++ data_setup.py
@@ -0,0 +1,57 @@
+"""
+Script for the syncing of the extracted frames (given by video_to_frames.py) 
+and the timestap of the ADVIO dataset.
+"""
+import pandas as pd
+import numpy as np
+import os
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+def resampling(sample: np.ndarray, frq_new:int) -> np.ndarray:
+    time_unit = 1/frq_new
+    resampled = []
+
+    old_time = 0
+    current_time = old_time + time_unit
+
+    for idx, s in enumerate(sample):
+        if current_time < s and idx != 0:
+            # memorizzo il frame piu vicino tra il min e il max
+            if abs(sample[idx-1] - current_time) < abs(sample[idx] - current_time):
+              resampled.append(sample[idx-1] )
+            else:
+              resampled.append(sample[idx])
+                
+            current_time += time_unit
+    
+    return np.array(resampled)
+
+input_file = "./data/advio-01/iphone/frames.csv"
+output_file = "./data/advio-01/iphone/frames_sanitize.csv"
+path_frames = "./data/advio-01/iphone/frames"
+
+print("Set up...")
+
+df = pd.read_csv(input_file, header=None)
+list_dir = os.listdir(path_frames)
+list_dir = np.array(list_dir)
+
+# timestamps at 60Hz
+ts = df.iloc[:, 0].to_numpy()
+
+# resampling to 50Hz
+tsr = resampling(ts, 50)
+
+# selecting the frames that matches timestamp
+frames = df.iloc[:, 1].to_numpy()
+frames = frames[np.where(np.isin(ts,tsr))]
+frame_names = np.array([f"{f}.jpg" for f in frames])
+
+data = np.stack([tsr, frame_names])
+df = pd.DataFrame(data.T)
+
+# saving sanitized df
+df.to_csv(output_file, index=False)
+
+print("Done!")
