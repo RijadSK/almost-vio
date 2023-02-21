2e661b8b0c328ed8f64932dc624cef93912557b7
fmolivato <f.olivato.97@gmail.com>
2023-02-21 22:03:18 +0100

    Extract the entire ADVIO dataset at once It was added a for to loop over all the folders

--- video_to_frames.py
+++ video_to_frames.py
@@ -6,48 +6,53 @@ import cv2
 import os
 import time
 
-start_time = time.time()
+for i in range(1, 24):
+    start_time = time.time()
 
-path = "./data/advio-01/iphone/"
-file_extension = ".mov"
-filename = path + "frames"+ file_extension
+    path = f"./data/advio-{i:02}/iphone/"
+    file_extension = ".mov"
+    filename = path + "frames" + file_extension
 
-if not os.path.exists(path+"frames/"):
-    os.mkdir(path+"frames/")
+    if not os.path.exists(path + "frames/"):
+        os.mkdir(path + "frames/")
 
-vid = cv2.VideoCapture(filename)
-fps = vid.get(cv2.CAP_PROP_FPS)
-frame_count = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    vid = cv2.VideoCapture(filename)
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    frame_count = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-print(f"""\nExtracting: 
-\t{filename}
-\t{fps} fps
-\t{frame_count} frames\n""")
+    print(
+        f"""\nExtracting: 
+  \t{filename}
+  \t{fps} fps
+  \t{frame_count} frames\n"""
+    )
 
-output_size = (224, 224)
+    output_size = (224, 224)
 
-if vid.isOpened():
-    for i in range(frame_count):
-                    
-        try:
-          if cv2.waitKey(10) & 0xFF == ord('q'):
-            print("Quit signal cv2")
-            break
+    if vid.isOpened():
+        for i in range(frame_count):
 
-          ret,frame = vid.read()
-          resized_frame = cv2.resize(frame, output_size, interpolation = cv2.INTER_AREA)
+            try:
+                if cv2.waitKey(10) & 0xFF == ord("q"):
+                    print("Quit signal cv2")
+                    break
 
-          cv2.imwrite(path+"frames/"+f"{i}.jpg", resized_frame)
-        except Exception as e:
-          print(f"Exception occurred: {e}")
-          break
+                ret, frame = vid.read()
+                resized_frame = cv2.resize(
+                    frame, output_size, interpolation=cv2.INTER_AREA
+                )
 
-        print(f"Completed {i/(frame_count+1)*100:1.2f} %", end="\r")
+                cv2.imwrite(path + "frames/" + f"{i}.jpg", resized_frame)
+            except Exception as e:
+                print(f"Exception occurred: {e}")
+                break
 
-end_time = time.time()
-elapsed_time = end_time - start_time
+            print(f"Completed {i/(frame_count+1)*100:1.2f} %", end="\r")
 
-print(f'\nIt took {elapsed_time/60:0.0f}min')
-print(f"Check the images at {path+'frames'}")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-vid.release()
+    print(f"\nIt took {elapsed_time/60:0.0f}min")
+    print(f"Check the images at {path+'frames'}")
+
+    vid.release()
